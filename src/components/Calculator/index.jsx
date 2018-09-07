2bb0eeed664c606e4efc35fc79c80ed3c47ef527
Aniruddha Shevle <aniruddhashevle@gmail.com>
2018-09-07 19:01:37 +0530

    folders with reset styling added and removed unneccessary files

---
+++ src/components/Calculator/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div>Yey</div>
+    }
+}
+
+export default Calculator;
